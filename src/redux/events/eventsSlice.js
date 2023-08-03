31831ae9332bfaa7b3eb60bee6e612621262df70
Irina Khorunz <irahorunz87@gmail.com>
2023-08-03 13:45:27 +0300

    add actions

--- src/redux/events/eventsSlice.js
+++ src/redux/events/eventsSlice.js
@@ -10,7 +10,46 @@ const eventsInitialState = {
 const eventsSlice = createSlice({
   name: 'events',
   initialState: eventsInitialState,
-  reducers: {},
+  reducers: {
+    addEvent: {
+      reducer(state, action) {
+        state.events.push(action.payload);
+      },
+      prepare(event) {
+        const {
+          title,
+          description,
+          date,
+          time,
+          location,
+          category,
+          picture,
+          priority,
+        } = event;
+
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            description,
+            date,
+            time,
+            location,
+            category,
+            picture,
+            priority,
+          },
+        };
+      },
+    },
+    deleteEvent(state, action) {
+      const index = state.events.findIndex(
+        event => event.id === action.payload
+      );
+      state.splice(index, 1);
+    },
+  },
 });
 
+export const { addEvent, deleteEvent } = eventsSlice.actions;
 export const eventsReducer = eventsSlice.reducer;
