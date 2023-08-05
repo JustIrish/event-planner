5bc7118b76b3d3e6075011ad03e56cd14af41f44
Irina Khorunz <irahorunz87@gmail.com>
2023-08-05 22:27:52 +0300

    update ActionBar

--- src/components/ActionBar/ActionBar.js
+++ src/components/ActionBar/ActionBar.js
@@ -1,21 +1,31 @@
 import { useLocation } from 'react-router-dom';
 
+import useWindowDimensions from 'hooks/useWindowDimensions';
 import Button from 'components/Buttons/Button';
-import { ButtonWrap } from './ActionBar.styled';
-import { BsPlusLg } from 'react-icons/bs';
+import { ButtonsContainer, BtnWrap } from './ActionBar.styled';
+import { BsPlusLg, BsSliders } from 'react-icons/bs';
+import { CiFilter } from 'react-icons/ci';
 
 const ActionBar = () => {
   const location = useLocation();
+  const { width } = useWindowDimensions();
 
   return (
-    <ButtonWrap>
+    <ButtonsContainer>
+      <BtnWrap>
+        {width > 767 && 'Category'} <CiFilter size="24" color="#3F3F3F" />
+      </BtnWrap>
+      <BtnWrap>
+        {width > 767 && 'Sort by'}
+        <BsSliders size="22" color="#3F3F3F" />
+      </BtnWrap>
       <Button
         type="button"
         title={<BsPlusLg size="24" color="#fff" />}
         to={`/create`}
         location={location}
       />
-    </ButtonWrap>
+    </ButtonsContainer>
   );
 };
 
--- src/components/ActionBar/ActionBar.styled.js
+++ src/components/ActionBar/ActionBar.styled.js
@@ -1,13 +1,34 @@
 import styled from 'styled-components';
-// import { mediaQueries } from 'styles/mediaQueries';
+import { mediaQueries } from 'styles/mediaQueries';
 
-export const ButtonWrap = styled.div`
+export const ButtonsContainer = styled.div`
   display: flex;
   justify-content: flex-end;
+  gap: 24px;
 
   a {
     width: 56px;
     height: 56px;
     padding: 16px;
+  }
+`;
+
+export const BtnWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 56px;
+  padding: 16px;
+  border-radius: 8px;
+  box-shadow: 2px 4px 9px 0px rgba(166, 141, 174, 0.28);
+
+  font-size: 16px;
+  font-weight: 500;
+  color: #3f3f3f;
+
+  svg {
+    ${mediaQueries('tablet')`
+     margin-left: 16px;`};
   }
 `;
